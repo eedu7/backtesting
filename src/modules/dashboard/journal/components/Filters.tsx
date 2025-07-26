249a7e1d61705315512d201c4bee9e88150b9774
eedu7 <mueedahmad067@gmail.com>
2025-07-26 12:54:35 +0500

    Added the filters options

--- src/modules/dashboard/journal/components/Filters.tsx
+++ src/modules/dashboard/journal/components/Filters.tsx
@@ -1,44 +1,33 @@
 import React from "react";
 
 import { FilterCard } from "@/modules/dashboard/journal/components/FilterCard";
-import { SkeletonFilterCard } from "@/modules/dashboard/journal/components/SkeletonFilterCard";
-import { coins } from "@/modules/dashboard/journal/constants";
+import { tradeDurations, tradeSides, tradeSymbolPairs, tradeTags } from "@/modules/dashboard/journal/constants";
 
 import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectLabel,
-    SelectTrigger,
-    SelectValue,
-} from "@/components/ui/select";
 
 export const Filters = () => {
     return (
         <div className="flex items-center gap-4 p-2">
             <div className="flex items-center gap-4">
                 <FilterCard
-                    label="Symbol"
-                    placeholder="Select a symbol"
-                    values={coins}
+                    label="Symbol Pair"
+                    placeholder="Select a symbol Pair"
+                    values={tradeSymbolPairs}
                 />
                 <FilterCard
                     label="Tags"
                     placeholder="Select a tag"
-                    values={coins}
+                    values={tradeTags}
                 />
                 <FilterCard
                     label="Side"
                     placeholder="Select a side"
-                    values={coins}
+                    values={tradeSides}
                 />
                 <FilterCard
                     label="Duration"
                     placeholder="Select a duration"
-                    values={coins}
+                    values={tradeDurations}
                 />
             </div>
             <div className="mt-[22px] flex items-center gap-4">
