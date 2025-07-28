e2b49c6c78051807ea5487e24b2a07746e63236c
eedu7 <mueedahmad067@gmail.com>
2025-07-28 13:05:11 +0500

    Added the drizzle connection

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import "drizzle-orm/node-postgres";
+
+import { Client } from "pg";
+
+const PG_HOST = process.env.PG_HOST;
+const PG_PORT = process.env.PG_PORT;
+const PG_USER = process.env.PG_USER;
+const PG_PASSWORD = process.env.PG_PASSWORD;
+const PG_DATABASE = process.env.PG_DATABASE;
+
+const client = new Client({
+    host: PG_HOST,
+    port: PG_PORT,
+    user: PG_USER,
+    password: PG_PASSWORD,
+    database: PG_DATABASE,
+});
+
+await client.connect();
+
+export const db = drizzle(client);
